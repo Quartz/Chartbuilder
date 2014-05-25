f233cdad07f8a52fba82a4dfaafa76fd387bd61a
yanofsky <github@yerit.com>
2014-05-25 18:26:53 +0200

    Chrome devs broke file naming so now we use blobs and some @shancarter SVGcrowbar action

--- js/chartbuilder.js
+++ js/chartbuilder.js
@@ -320,18 +320,83 @@ ChartBuilder = {
 			});
 			
 			
-		// Create SVG image
-		var svgString = $("#chartContainer").html();
-		//add in all the things that validate SVG
-		svgString = '<?xml version="1.0" encoding="utf-8"?>\n<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n<svg ' + svgString.split("<svg ")[1];
+		var svgContent = this.createSVGContent(document.getElementById("chart"));
 		
-	$("#downloadSVGLink").attr("href","data:text/svg,"+ encodeURI(svgString.split("PTSerif").join("PT Serif")) )
-		.attr("download",function(){ return filename + "_chartbuilder.svg";
-		});
+		$("#downloadSVGLink").attr("href","data:text/svg,"+ svgContent.source[0])
+			.attr("download",function(){ return filename + "_chartbuilder.svg";});
 
-		var icon = this.setFavicon();
-		this.storeLocalChart(filename);
+			var icon = this.setFavicon();
+			this.storeLocalChart(filename);
+
+		if(!(/Apple/).test(navigator.vendor)) {
+			//blobs dont work in Safari so don't use that method
+
+			var link = document.getElementById('downloadImageLink');
+			var base64 = canvas.toDataURL("png").split(",")[1];
+			var bytes = window.atob(base64);
+			var ui8a = new Uint8Array(bytes.length);
+
+			for (var i = 0; i < bytes.length; i++)
+				ui8a[i] = bytes[i].charCodeAt(0);
+
+			var blob = new Blob([ui8a], { type: 'image/png' });
+			var url = URL.createObjectURL(blob);
+			link.href = url;
+			
+			link = document.getElementById('downloadSVGLink');
+			blob = new Blob(svgContent.source, { type: '"text\/xml"' });
+			url = URL.createObjectURL(blob);
+			link.href = url;
+		}
 		
+	},
+	createSVGContent: function(svg) {
+		/*
+			Copyright (c) 2013 The New York Times
+
+			Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+			The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+			SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+		*/
+
+		//via https://github.com/NYTimes/svg-crowbar
+
+		var prefix = {
+			xmlns: "http://www.w3.org/2000/xmlns/",
+			xlink: "http://www.w3.org/1999/xlink",
+			svg: "http://www.w3.org/2000/svg"
+		};
+
+		var doctype = '<?xml version="1.0" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">';
+
+
+		svg.setAttribute("version", "1.1");
+
+		var defsEl = document.createElement("defs");
+		svg.insertBefore(defsEl, svg.firstChild); //TODO   .insert("defs", ":first-child")
+
+		var styleEl = document.createElement("style");
+		defsEl.appendChild(styleEl);
+		styleEl.setAttribute("type", "text/css");
+
+
+		// removing attributes so they aren't doubled up
+		svg.removeAttribute("xmlns");
+		svg.removeAttribute("xlink");
+
+		// These are needed for the svg
+		if (!svg.hasAttributeNS(prefix.xmlns, "xmlns")) {
+			svg.setAttributeNS(prefix.xmlns, "xmlns", prefix.svg);
+		}
+
+		if (!svg.hasAttributeNS(prefix.xmlns, "xmlns:xlink")) {
+			svg.setAttributeNS(prefix.xmlns, "xmlns:xlink", prefix.xlink);
+		}
+
+		var source = (new XMLSerializer()).serializeToString(svg).replace('</style>', '<![CDATA[' + styles + ']]></style>');
+
+		return {svg: svg, source: [doctype + source]};
 	},
 	setFavicon: function() {
 		//set favicon to image of chart
