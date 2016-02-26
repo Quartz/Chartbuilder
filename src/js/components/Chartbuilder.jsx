fd8e23f95a5e1c6f1e4458390b3ba58a381d51cc
yanofsky <github@yerit.com>
2016-02-25 17:26:02 -0800

    make sure the number of steps is correct with date and numeric series

--- src/js/components/Chartbuilder.jsx
+++ src/js/components/Chartbuilder.jsx
@@ -140,7 +140,7 @@ var Chartbuilder = React.createClass({
 			mobileOverrides = null;
 		}
 
-		var editorSteps = Editor.defaultProps.numSteps;
+		var editorSteps = Editor.defaultProps.numSteps + (this.state.chartProps.hasDate || this.state.chartProps.isNumeric ? 1 : 0);
 		var mobilePreview;
 
 		// Mobile preview of the chart, if told to render
