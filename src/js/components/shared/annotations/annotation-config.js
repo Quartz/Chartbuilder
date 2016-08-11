de0a23539c2b74840592065befa275ac0bf9b30c
David Yanofsky <github@yerit.com>
2016-08-10 17:02:33 -0700

    adding annotation-config.js

---
+++ src/js/components/shared/annotations/annotation-config.js
@@ -0,0 +1,14 @@
+var annotation_config = {
+	defaultBlurb: {
+		tout: "New Blurb",
+		copy: "Lorem ipsum dolor sit amet.",
+		pos: {x: 100, y: 100},
+		arrowStart: {x:0.5, y:0.5},
+		arrowEnd: {x:0.5, y:0.8},
+		arrowClockwise: true
+	}
+}
+
+
+
+module.exports = annotation_config
